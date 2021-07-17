04e38493bd0f98a47cf2eb117601ba506cd848f6
gengsen <gengsenyi@vip.qq.com>
2021-07-17 11:37:05 +0800

    feat: feat: init version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
   root: true,
-  plugins: ['prettier'], // ++
-  extends: [
-    'plugin:vue/vue3-recommended', // ++
-    'airbnb-base', // ++
-    'plugin:prettier/recommended', // ++
-  ],
+  plugins: ['prettier'],
+  extends: ['plugin:vue/vue3-recommended', 'airbnb-base', 'plugin:prettier/recommended'],
   env: {
     browser: true, // browser global variables
-    es2021: true, // adds all ECMAScript 2021 globals and automatically sets the ecmaVersion parser option to 12.
+    es2021: true // adds all ECMAScript 2021 globals and automatically sets the ecmaVersion parser option to 12.
   },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 12
   },
   rules: {
-    'prettier/prettier': 'error', // ++
-  },
+    'prettier/prettier': 'error'
+  }
 }
